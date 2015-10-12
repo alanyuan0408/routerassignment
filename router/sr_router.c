1daecce59ae19a0492d5b83cff75d7e31b828115
Alan <alan.yuan@mail.utoronto.ca>
2015-10-12 16:56:29 -0400

    fixed print

--- router/sr_router.c
+++ router/sr_router.c
@@ -97,7 +97,7 @@ void arp_handlepacket(uint8_t * packet) {
   printf("** Recieved ARP packet");
   /* Initialization */
   sr_arp_hdr_t *arp_hdr = arp_header(packet);
-  printf(ntohs(arp_hdr->ar_hrd))
+  fprintf(stderr, "\thardware type: %d\n", ntohs(arp_hdr->ar_hrd));
 
 }
 
