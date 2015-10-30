55a187239c1a7a2bec0098825c603c5181b86775
Alan <alan.yuan@mail.utoronto.ca>
2015-10-30 17:24:16 -0400

    ICMP header

--- router/sr_router.c
+++ router/sr_router.c
@@ -471,7 +471,7 @@ struct sr_icmp_hdr icmp_send_reply_packet()
 	return icmp_echo_reply;
 }
 
-struct sr_icmp_t3_hdr icmp_send_error_packet(struct sr_ip_hdr *ip_hdr, int code_num)
+struct sr_icmp_t3_hdr *icmp_send_error_packet(struct sr_ip_hdr *ip_hdr, int code_num)
 {
 
   	struct sr_icmp_t3_hdr *icmp_error_reply;
