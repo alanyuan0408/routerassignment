29e91ea4e88c550e91dee84d088bfde62e8a34f0
Alan <alan.yuan@mail.utoronto.ca>
2015-10-30 18:20:41 -0400

    test

--- router/sr_router.c
+++ router/sr_router.c
@@ -154,8 +154,9 @@ void arp_handlepacket(struct sr_instance *sr,
 
         /* Check ARP cache  */
         arp_entry = sr_arpcache_lookup(&sr->cache, arp_hdr->ar_sip);
-        if (arp_entry != NULL){
+        if (arp_entry != 0){
           printf("ARP entry is already in the cache\n");
+          free(arp_entry);
         }else {
           arp_req = sr_arpcache_insert(&sr->cache, arp_hdr->ar_sha, arp_hdr->ar_sip);
           printf("ARP insert entry\n");
