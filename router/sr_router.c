6ceca955230530c4d00e999f3e77eaa95bbef4b0
Alan <alan.yuan@mail.utoronto.ca>
2015-10-31 18:58:02 -0400

    test size

--- router/sr_router.c
+++ router/sr_router.c
@@ -251,7 +251,7 @@ void ip_handlepacket(struct sr_instance *sr,
 
             uint16_t icmp_len;
             icmp_len = total_len - ICMP_IP_HDR_LEN;
-            fprintf(stderr, "%d.\n", icmp_len);
+            fprintf(stderr, "%d.\n", sizeof(sr_icmp_hdr_t));
 
             icmp_hdr_ptr = icmp_header((struct sr_ip_hdr *)cache_packet);
             icmp_hdr_ptr->icmp_sum = cksum(icmp_hdr_ptr, sizeof(sr_icmp_hdr_t));
