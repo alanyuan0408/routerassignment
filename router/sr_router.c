1733abe9cfb3ccc02cd49ba79d34b26a7c77f997
Alan <alan.yuan@mail.utoronto.ca>
2015-11-05 19:30:56 -0500

    1

--- router/sr_router.c
+++ router/sr_router.c
@@ -345,7 +345,7 @@ void ip_handlepacket(struct sr_instance *sr,
             send_ip_hdr.ip_len = htons(total_len);
             send_ip_hdr.ip_sum = cksum(&send_ip_hdr, ICMP_IP_HDR_LEN_BYTE);
             /* Modify the ICMP error packet */
-	          icmp_error_packet = icmp_send_error_packet(ip_hdr, 3);
+	          icmp_error_packet = icmp_send_error_packet(ip_hdr, 3); 
 
             icmp_len = sizeof(struct sr_icmp_t3_hdr);
             total_len = ICMP_IP_HDR_LEN_BYTE + icmp_len;
@@ -499,22 +499,24 @@ void sr_handle_arpreq(struct sr_instance *sr, struct sr_arpreq *req)
             return;
           }
 
+          struct sr_icmp_t3_hdr icmp_error_packet = icmp_send_error_packet(ip_hdr,1);
+
+
           uint32_t dst;
           dst = ip_hdr->ip_src;
           ip_hdr->ip_src = ip_hdr->ip_dst;
           ip_hdr->ip_dst = dst;
-
-          /* Modify the ICMP error packet */
-  	      struct sr_icmp_t3_hdr icmp_error_packet = icmp_send_error_packet(ip_hdr,1);
-
+  	      
           /* Copy the packet over */
           uint8_t *cache_packet;
           uint16_t total_len;
 
           total_len = ip_len(ip_hdr);
           cache_packet = malloc(total_len);
-          memcpy(cache_packet, ip_hdr, (ip_hdr->ip_hl)*4);
-          memcpy(cache_packet, &(icmp_error_packet), sizeof(sr_icmp_t3_hdr_t));
+
+          memcpy(cache_packet, ip_hdr, ICMP_IP_HDR_LEN_BYTE);
+          memcpy(cache_packet + ICMP_IP_HDR_LEN_BYTE, &(icmp_error_packet), 
+              sizeof(struct sr_icmp_t3_hdr));
 
           struct sr_arpreq *req;
           struct sr_arpentry *arp_entry;
